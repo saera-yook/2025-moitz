fca525cb8b988537c51dd310f38560d5b2eef35d
saera <yook.saera@gmail.com>
2025-09-21 15:55:05 +0900

    fix: 더미 데이터 파싱 오류 해결

--- backend/src/main/java/com/f12/moitz/infrastructure/client/perplexity/PerplexityClient.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/client/perplexity/PerplexityClient.java
@@ -66,13 +66,13 @@ public class PerplexityClient {
                         "recommendations": [
                             {
                                 "locationName": "신촌역",
-                                "reason": "접근성 좋고 맛집이 많아요! 😋"
-                                "description": "접근성 좋고 맛집이 많아요! 😋"
+                                "detail_reason": "접근성 좋고 맛집이 많아요! 😋",
+                                "summarize_reason": "접근성 좋고 맛집이 많아요! 😋"
                             },
                             {
                                 "locationName": "이대역",
-                                "reason": "학생들이 많아 맛집이 많아요! 🍜"
-                                "description": "학생들이 많아 맛집이 많아요! 🍜"
+                                "detail_reason": "학생들이 많아 맛집이 많아요! 🍜",
+                                "summarize_reason": "학생들이 많아 맛집이 많아요! 🍜"
                             }
                         ]
                     }
