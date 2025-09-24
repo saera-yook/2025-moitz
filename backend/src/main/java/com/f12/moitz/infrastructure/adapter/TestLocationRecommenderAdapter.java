6653cdbd5502d30878e306c0e07b7b368b1bf0e6
saera <yook.saera@gmail.com>
2025-09-24 16:03:06 +0900

    feat: 서킷브레이커 적용

--- backend/src/main/java/com/f12/moitz/infrastructure/adapter/TestLocationRecommenderAdapter.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/adapter/TestLocationRecommenderAdapter.java
@@ -2,10 +2,16 @@ package com.f12.moitz.infrastructure.adapter;
 
 import com.f12.moitz.application.dto.RecommendedLocationsResponse;
 import com.f12.moitz.application.port.LocationRecommender;
+import com.f12.moitz.common.error.exception.ExternalApiException;
 import com.f12.moitz.common.error.exception.RetryableApiException;
 import com.f12.moitz.infrastructure.client.gemini.GoogleGeminiClient;
 import com.f12.moitz.infrastructure.client.perplexity.PerplexityClient;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.decorators.Decorators;
+import java.time.Duration;
 import java.util.List;
+import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Recover;
@@ -30,12 +36,48 @@ public class TestLocationRecommenderAdapter implements LocationRecommender {
             final List<String> startPlaceNames,
             final String condition
     ) {
-        final RecommendedLocationsResponse generatedResponse = geminiClient.generateForTest("Say \"Hello, world!\"");
+        CircuitBreakerConfig configForQuota = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(10000))
+                .permittedNumberOfCallsInHalfOpenState(2)
+                .slidingWindowSize(1)
+                .minimumNumberOfCalls(1)
+                .recordExceptions(ExternalApiException.class)
+                .ignoreExceptions(RetryableApiException.class)
+                .build();
+
+        CircuitBreaker breakerForQuota = CircuitBreaker.of("breakerForQuota", configForQuota);
+
+        CircuitBreakerConfig configForRetryable = CircuitBreakerConfig.custom()
+                .failureRateThreshold(60)
+                .waitDurationInOpenState(Duration.ofMillis(10000))
+                .permittedNumberOfCallsInHalfOpenState(2)
+                .slidingWindowSize(5)
+                .minimumNumberOfCalls(1)
+                .recordExceptions(RetryableApiException.class)
+                .ignoreExceptions(ExternalApiException.class)
+                .build();
+
+        CircuitBreaker breakerForRetryable = CircuitBreaker.of("breakerForRetryable", configForRetryable);
+
+        Supplier<RecommendedLocationsResponse> decoratedSupplier = Decorators.ofSupplier(() -> geminiClient.generateForTest("Say \"Hello, world!\""))
+                .withCircuitBreaker(breakerForQuota)
+                .withCircuitBreaker(breakerForRetryable)
+                .withFallback(List.of(ExternalApiException.class), throwable -> fallback())
+                .decorate();
+
+        final RecommendedLocationsResponse generatedResponse = decoratedSupplier.get();
+
         final RecommendedLocationsResponse deduplicatedLocations = deduplicateLocation(generatedResponse);
         return excludeStartPlaces(
                 deduplicatedLocations,
                 startPlaceNames
         );
+    }
+
+    private RecommendedLocationsResponse fallback() {
+        log.info("FallBack: Gemini 호출 응답 실패. Perplexity 호출을 시도합니다.");
+        return perplexityClient.generateForTest("Say \"Hello, world!\"");
     }
 
     @Recover
