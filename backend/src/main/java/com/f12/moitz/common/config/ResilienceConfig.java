cc46b97db97a760e58f6b7175445f9c073a24ffc
saera <yook.saera@gmail.com>
2025-09-25 21:48:18 +0900

    fix: metric 수집을 위한 설정 수정

--- backend/src/main/java/com/f12/moitz/common/config/ResilienceConfig.java
+++ backend/src/main/java/com/f12/moitz/common/config/ResilienceConfig.java
@@ -3,6 +3,9 @@ package com.f12.moitz.common.config;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.event.CircuitBreakerOnIgnoredErrorEvent;
+import io.github.resilience4j.micrometer.tagged.TaggedCircuitBreakerMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +18,15 @@ public class ResilienceConfig {
 
     @Bean
     public CircuitBreaker geminiBreaker() {
+        MeterRegistry meterRegistry = new SimpleMeterRegistry();
+
+        TaggedCircuitBreakerMetrics
+                .ofCircuitBreakerRegistry(registry)
+                .bindTo(meterRegistry);
+
         final CircuitBreaker circuitBreaker = registry.circuitBreaker("gemini");
         circuitBreaker.getEventPublisher();
+
         return circuitBreaker;
     }
 
